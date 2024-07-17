fe6a4577527afdf3c12b7147845b9c8c8ef8c52e
Ellinor Kwok <ekwok@genymobile.com>
2024-07-17 17:30:55 +0200

    Create the first version of the script

---
+++ generate_sorted_logs.py
@@ -0,0 +1,69 @@
+import glob
+import re
+import os
+from datetime import datetime
+
+# Extracts timestamp from a line of a log
+def extract_timestamp(log_line):
+    # Regex for "MM-DD HH:MM:SS.mmm"
+    match = re.search(r'\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}', log_line)
+    if match:
+        timestamp_str = match.group()
+        return datetime.strptime('2024-' + timestamp_str, '%Y-%m-%d %H:%M:%S.%f')
+
+    # Regex for "YYYY-MM-DDTHH:MM:SSZ" or "YYYY-MM-DDTHH:MM:SS.mmmZ" or "MM-DD HH:MM:SS.mmm"
+    match = re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d{3})?(?:Z)?', log_line)
+    if match:
+        timestamp_str = match.group()
+        # Remove the 'Z' at the end if present
+        if timestamp_str.endswith('Z'):
+            timestamp_str = timestamp_str[:-1]
+        if '.' in timestamp_str:
+            return datetime.strptime(timestamp_str, '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            return datetime.strptime(timestamp_str, '%Y-%m-%dT%H:%M:%S')
+
+#Creates the complete output file path for the aggregated logs - Will create an output directory if different search patterns need to be stored at the same place
+#Returns its complete path
+def create_output_file (output_file_name):
+
+    # Get current path
+    log_dir = os.getcwd()
+    
+    # Checks if the output directory exists otherwise creates it
+    output_directory =  os.getcwd() + "/output"
+    print(f"Sorted logs are generated in {output_directory}")
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+
+    # Gets the absolute output file path
+    return os.path.join(output_directory, output_file_name)
+    
+search_patterns = [] #To fill if those logs needed to be filtered
+
+# Read all log files
+scanned_log_files = glob.glob(os.getcwd()+ '/*.log')
+log_entries = []
+
+for log_file in scanned_log_files:
+    print(f"Reading log file: {log_file}")  # Displays the scanned log files
+    with open(log_file, 'r') as file:
+        for line in file:
+            if not search_patterns or any(pattern in line for pattern in search_patterns): #handles if there's no need to filter the logs
+                timestamp = extract_timestamp(line)
+                if timestamp:
+                    log_entries.append((timestamp, line))
+
+# Sort logs by timestamps
+sorted_logs = sorted(log_entries, key=lambda x: x[0])
+
+output_file_path = create_output_file('sorted_logs.log')
+
+with open(output_file_path, 'w') as output_file:
+    for entry in sorted_logs:
+        output_file.write(entry[1])
+
+if search_patterns:
+    print(f"All sorted logs matching '{search_patterns}' are written in {output_file_path}")
+else: 
+    print(f"All sorted logs are written in {output_file_path}")
